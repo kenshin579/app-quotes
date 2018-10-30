fa77e4038c4182837a5c9004c9082c7a76aefabd
ykoh <kenshin579@gmail.com>
2018-10-30 23:07:39 +0900

    modify python script - send bulk quotes files

--- scripts/upload_best_quotes.py
+++ scripts/upload_best_quotes.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import argparse
+import datetime
+import json
+import os
+import pprint
+import sys
 
+import requests
 
 ################################################################################################
-# quotes를 API 서버에 upload하는 script
 # TODO:
 ################################################################################################
 
@@ -12,30 +18,68 @@
 # Constants
 #
 ################################################################################################
-import sys
+URL_QUOTE_SERVER = "http://localhost:8080/api/v1/quotes"
+DEFAULT_LANG = "korean"
 
-URL_QUOTE_SERVER = "http://localhost:8080/ap/v1/new"
+
+################################################################################################
+# Functions
+#
+################################################################################################
+def parse_quote_file(filename, tags, lang=DEFAULT_LANG):
+    print(filename)
+    result = []
+    with open(filename) as f:
+        for line in f:
+            if len(line.strip()) != 0 and not line.startswith("#"):
+                lines = line.split("-")
+                result.append({
+                    "quote": lines[0].strip(),
+                    "author": lines[1].strip(),
+                    "created": datetime.datetime.today().strftime('%Y-%m-%d'),
+                    "tags": tags,
+                    "lang": lang
+                })
+    return result
+
+
+def send_one_quote(url, quote):
+    pprint.pprint(quote)
+    headers = {'Content-Type': 'application/json; charset=utf-8'}
+    r = requests.post(url, data=json.dumps(quote), headers=headers)
+    print("r", r)
+
+
+def send_bulk_quotes(url, quote_list):
+    headers = {'Content-Type': 'application/json; charset=utf-8'}
+    r = requests.post(url, data=json.dumps(quote_list), headers=headers)
+    print("r", r)
 
 
 ################################################################################################
 # Main function
 #
 ################################################################################################
-import argparse
-
 
 def main():
-    parser = argparse.ArgumentParser(description="Uploading quotes to API")
+    parser = argparse.ArgumentParser(description="Uploading quotes to API server: " + URL_QUOTE_SERVER)
 
-    parser.add_argument("-u", "--upload", action='store_true',
-                        help="sending quotes to " + URL_QUOTE_SERVER)
-
-    print(parser.parse_args())
+    parser.add_argument("-t", "--tags", nargs='+',
+                        help="sending quotes to " + URL_QUOTE_SERVER, required=True)
+    parser.add_argument("-f", "--file", action="store", dest="filename", help="input file for quote", required=True)
 
     args = parser.parse_args()
+    print(args)
 
-    if args.upload:
+    if args.filename:
         print("uploading...")
+        if os.path.exists(args.filename):
+            send_bulk_quotes(URL_QUOTE_SERVER, parse_quote_file(args.filename, args.tags))
+
+            # test
+            # send_one_quote(URL_QUOTE_SERVER, parse_quote_file(args.filename, args.tags)[0])
+        else:
+            print("filename not found: " + args.filename)
 
 
 if __name__ == "__main__":
