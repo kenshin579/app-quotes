8dc654a62769e943e8d536d1d561ab563020e9ad
frankyoh <frankyoh@tmon.co.kr>
2019-11-12 21:47:53 +0900

    [QUOTE-2][BE] 프로젝트 다시 cleanup - 구동시 이상없음

---
+++ src/main/java/kr/pe/advenoh/quote/constants/AppConstants.java
@@ -0,0 +1,8 @@
+package kr.pe.advenoh.quote.constants;
+
+public interface AppConstants {
+    String DEFAULT_PAGE_NUMBER = "0";
+    String DEFAULT_PAGE_SIZE = "30";
+
+    int MAX_PAGE_SIZE = 50;
+}
