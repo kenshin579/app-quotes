855d8a7bf7ebef7e35c0f1e2e53203208d2cf03f
frankyoh <frankyoh@tmon.co.kr>
2020-03-15 22:51:44 +0900

    [QUOTE-2][BE] UserDetailService 파일 추가

---
+++ src/main/java/kr/pe/advenoh/quote/spring/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package kr.pe.advenoh.quote.spring.security;
+
+import kr.pe.advenoh.quote.exception.ResourceNotFoundException;
+import kr.pe.advenoh.quote.model.entity.User;
+import kr.pe.advenoh.quote.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username or email : " + usernameOrEmail));
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
+        return UserPrincipal.create(user);
+    }
+}
