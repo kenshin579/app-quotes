b85a07fe2205e44e5bf280762f0c549671f69f63
frankyoh <frankyoh@tmon.co.kr>
2020-03-22 17:06:28 +0900

    [QUOTE-2][FE] body 부분에 내용 보이도록 scss 추가

--- app/src/components/common/PageTemplate/PageTemplate.js
+++ app/src/components/common/PageTemplate/PageTemplate.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import styles from './PageTemplate.scss';
 import Header from "../AppHeader";
+import {Layout} from "antd";
+import classNames from 'classnames/bind';
+
+const {Content} = Layout;
+const cx = classNames.bind(styles);
 
 const PageTemplate = ({children}) => (
     <div>
-        <Header/>
-        <main>
-            {/*{children}*/}
-        </main>
+        <Layout>
+            <Header/>
+            <Content className={cx('page-template-content')}>
+                <div className={cx('container')}>
+                    {children}
+                </div>
+            </Content>
+        </Layout>
     </div>
 );
 
