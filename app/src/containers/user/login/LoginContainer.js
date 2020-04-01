0c22d925dc1ebb139cfc359ae7683f1d609ac9a0
frankyoh <frankyoh@tmon.co.kr>
2020-04-01 23:25:33 +0900

    [QUOTE-2][FE] 로그인관련 파일 추가

---
+++ app/src/containers/user/login/LoginContainer.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from "redux";
+import * as baseActions from 'store/modules/base';
+import Login from "../../../components/user/Login";
+import {ACCESS_TOKEN} from "../../../constants";
+import {notification} from 'antd';
+
+class LoginContainer extends Component {
+    handleFinish = async (values) => {
+        console.log('values', values);
+
+        const {BaseActions} = this.props;
+
+        try {
+            const response = await BaseActions.login(values);
+            localStorage.setItem(ACCESS_TOKEN, response.accessToken);
+
+        } catch (err) {
+            console.error('err', err);
+            if (err.status === 401) {
+                notification.error({
+                    message: 'Quote App',
+                    description: '아이디나 암호가 잘못 입력되었습니다. 다시 시도해주세요!',
+                });
+            } else {
+                notification.error({
+                    message: 'Quote App',
+                    description: '서버 오류가 발생했습니다. 사디 시도해주세요',
+                });
+            }
+        }
+    };
+
+    render() {
+        return (
+            <Login onFinish={this.handleFinish}/>
+        )
+    }
+}
+
+export default connect(
+    (state) => ({
+        authenticated: state.base.get('authenticated')
+    }),
+    (dispatch) => ({
+        BaseActions: bindActionCreators(baseActions, dispatch)
+    })
+)(LoginContainer);
