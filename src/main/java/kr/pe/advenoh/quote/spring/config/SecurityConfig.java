01fa85a97c6dca35ef1a7c22c9d75448d8fad2d0
frankyoh <frankyoh@tmon.co.kr>
2020-04-02 22:31:08 +0900

    [QUOTE-2][BE] @PreAuthorize 어노테이션 활성화를 위해 스프링 보완 설정 변경함

--- src/main/java/kr/pe/advenoh/quote/spring/config/SecurityConfig.java
+++ src/main/java/kr/pe/advenoh/quote/spring/config/SecurityConfig.java
@@ -5,10 +5,10 @@ import kr.pe.advenoh.quote.spring.security.JwtAuthenticationEntryPoint;
 import kr.pe.advenoh.quote.spring.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +18,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
