0bf016e29170cceaa169d7e672809d80c2dd6b09
frankyoh <frankyoh@tmon.co.kr>
2020-05-21 20:33:37 +0900

    [QUOTE-2][FE] AuthenticatedRoute 추가

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import {LoginPage, MainPage, MyQuote, NotFoundPage, SettingPage, SignUpPage} from 'pages';
+import {
+    LoginPage,
+    MainPage,
+    MyQuoteMainPage,
+    MyQuotePage,
+    NotFoundPage,
+    SettingPage,
+    SignUpPage,
+    TestPage
+} from 'pages';
+import BaseContainer from '../containers/common/BaseContainer';
+import AuthenticatedRoute from "./common/AuthenticatedRoute";
 
 const App = () => {
     return (
-        <div>
+        <>
             <Switch>
                 <Route exact path="/" component={MainPage}/>
                 <Route path="/signup" component={SignUpPage}/>
                 <Route path="/login" component={LoginPage}/>
-                <Route path="/quotes" component={MyQuote}/>
-                <Route path="/settings" component={SettingPage}/>
+                <AuthenticatedRoute exact path="/users/:username/quotes" component={MyQuoteMainPage}/>
+                <AuthenticatedRoute path="/users/:username/quotes/folders/:folderId" component={MyQuotePage}/>
+                <AuthenticatedRoute path="/users/:username/settings" component={SettingPage}/>
+                <Route path="/test" component={TestPage}/>
                 <Route component={NotFoundPage}/>
             </Switch>
-        </div>
+            <BaseContainer/>
+        </>
     );
 };
 
