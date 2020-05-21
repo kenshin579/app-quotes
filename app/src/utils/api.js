54fdded111318b4ca5f2062fb084e3b5f6112c91
frankyoh <frankyoh@tmon.co.kr>
2020-05-21 22:38:42 +0900

    [QUOTE-4][FE] API 추가

--- app/src/utils/api.js
+++ app/src/utils/api.js
@@ -1,9 +1,13 @@
 import {ACCESS_TOKEN} from "../constants";
 
 const request = (options) => {
-    const headers = new Headers({
+    let headers = new Headers({
         'Content-Type': 'application/json',
     });
+
+    if (options.body instanceof FormData) {
+        headers = new Headers();
+    }
 
     let token = localStorage.getItem(ACCESS_TOKEN);
     if (token) {
@@ -23,6 +27,11 @@ const request = (options) => {
             })
         );
 };
+
+const objectToQueryString = (obj) => {
+    return Object.keys(obj).map(key => key + '=' + obj[key]).join('&');
+};
+
 
 export const login = (loginRequest) => {
     console.log('loginRequest', loginRequest);
@@ -60,5 +69,66 @@ export const logout = () => {
     return request({
         url: '/api/auth/logout',
         method: 'POST'
+    });
+};
+
+export const getQuoteList = (id, pagination) => {
+    console.log('getRandomList :: id', id, 'pagination', pagination);
+
+    const queryStr = objectToQueryString({
+        pageIndex: pagination['current'],
+        pageSize: pagination['pageSize']
+    });
+
+    let url = `/api/quotes/folders/${id}?${queryStr}`;
+    console.log('url', url);
+
+    return request({
+        url: url,
+        method: 'GET'
+    });
+};
+
+export const getFolderList = () => {
+    return request({
+        url: '/api/folders',
+        method: 'GET'
+    });
+};
+
+export const createQuote = (folderId, quoteData) => {
+    const formData = new FormData();
+    for (const name in quoteData) {
+        formData.append(name, quoteData[name]);
+    }
+
+    return request({
+        url: '/api/quotes/folders/' + folderId,
+        method: 'POST',
+        body: formData
+    });
+};
+
+export const deleteQuotes = (selectedRowKeys) => {
+    console.log('selectedRowKeys', selectedRowKeys);
+    const quoteIds = selectedRowKeys.join(',');
+
+    return request({
+        url: `/api/quotes?quoteIds=${quoteIds}`,
+        method: 'DELETE',
+    });
+};
+
+export const getRandomList = (pagination) => {
+    console.log('getRandomList :: pagination', pagination);
+
+    const queryStr = objectToQueryString({
+        pageIndex: pagination['current'],
+        pageSize: pagination['pageSize']
+    });
+
+    return request({
+        url: `/api/quotes/today?${queryStr}`,
+        method: 'GET'
     });
 };
