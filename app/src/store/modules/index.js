c8cae46e3bab50a8d28a4251e5b39c1be26cfb8b
frankyoh <frankyoh@tmon.co.kr>
2020-05-21 22:43:50 +0900

    [QUOTE-5][FE] main action 정의

--- app/src/store/modules/index.js
+++ app/src/store/modules/index.js
@@ -1,2 +1,5 @@
 export {default as base} from './base';
+export {default as quote} from './quote';
+export {default as folder} from './folder';
+export {default as main} from './main';
 export {penderReducer as pender} from 'redux-pender';
