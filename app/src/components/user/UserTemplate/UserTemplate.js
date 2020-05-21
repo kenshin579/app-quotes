1855ecfcae227beaab57cb99b400b01d0bac1942
frankyoh <frankyoh@tmon.co.kr>
2020-05-21 20:36:45 +0900

    [QUOTE-4][FE] UserTemplate 추가

---
+++ app/src/components/user/UserTemplate/UserTemplate.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './UserTemplate.scss';
+import {Layout} from "antd";
+import classNames from 'classnames/bind';
+import AppHeaderContainer from "../../../containers/common/AppHeaderContainer";
+import SidebarContainer from "../../../containers/user/LeftSidebarContainer";
+
+const {Content} = Layout;
+const cx = classNames.bind(styles);
+
+const UserTemplate = ({children}) => (
+    <Layout>
+        <AppHeaderContainer/>
+        <Layout>
+            <SidebarContainer />
+            {children}
+        </Layout>
+    </Layout>
+);
+
+export default UserTemplate;
