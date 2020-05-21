7af9abf345956dc12c7e91f8a1b4bdfb1e0e993b
frankyoh <frankyoh@tmon.co.kr>
2020-05-21 20:40:55 +0900

    [QUOTE-4][FE] Activity 컨포넌트 추가 (작업이 필요함)

---
+++ app/src/components/user/list/ActivityList/ActivityList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './ActivityList.scss';
+import classNames from 'classnames/bind';
+import {Col, Divider, Layout, Row} from 'antd';
+import StatisticItem from "../StatisticItem";
+
+const cx = classNames.bind(styles);
+const Header = Layout.Header;
+
+const ActivityList = ({activityList}) => {
+    const statisticView = [];
+
+    // Object.keys(statisticInfo).forEach((key) => {
+    //     statisticView.push(
+    //         <StatisticItem key={key} title={key} data={statisticInfo[key]}/>
+    //     )
+    // });
+
+    return (
+        <Row>
+            <div className={cx('date')}>2019년 6월 14일</div>
+
+
+        </Row>
+    );
+};
+export default ActivityList;
