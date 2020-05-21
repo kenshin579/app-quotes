6bc34c602bd5484e147b6fbd2fcbfc3319d4a2a9
frankyoh <frankyoh@tmon.co.kr>
2020-05-21 20:04:36 +0900

    [QUOTE-4][BE] Quote 관련 ResponseDto

---
+++ src/main/java/kr/pe/advenoh/quote/model/dto/QuoteResponseDto.java
@@ -0,0 +1,34 @@
+package kr.pe.advenoh.quote.model.dto;
+
+import kr.pe.advenoh.quote.model.enums.YN;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+
+public class QuoteResponseDto {
+    private Long quoteId;
+    private String quoteText;
+    private String authorName;
+
+    //    @JsonProperty("userInfo")
+//    private UserDto user;
+    private YN useYn;
+
+    //todo : tags도 포함해야 할까?
+    //todo : 좋아요의 수도 포함해야 하지 않을까?
+
+    @Builder
+    public QuoteResponseDto(Long quoteId, String quoteText, String authorName, YN useYn) {
+        this.quoteId = quoteId;
+        this.quoteText = quoteText;
+        this.authorName = authorName;
+        this.useYn = useYn;
+    }
+}
