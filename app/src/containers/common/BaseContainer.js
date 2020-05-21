078686f62c24dce68f266618be332eeffbab2871
frankyoh <frankyoh@tmon.co.kr>
2020-05-21 20:47:32 +0900

    [QUOTE-4][FE] Modal 컨테이너 BaseContainer에 포함시킴

--- app/src/containers/common/BaseContainer.js
+++ app/src/containers/common/BaseContainer.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import QuoteCreateModalContainer from '../modal/QuoteCreateModalContainer';
+import QuoteDeleteModalContainer from '../modal/QuoteDeleteModalContainer';
 
 class BaseContainer extends Component {
     render() {
         return (
             <div>
+                <QuoteCreateModalContainer/>
+                <QuoteDeleteModalContainer/>
                 { /* 전역적으로 사용되는 컴포넌트들이 있다면 여기서 렌더링합니다. */}
             </div>
         )
