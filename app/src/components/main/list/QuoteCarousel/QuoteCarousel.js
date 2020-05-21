87dc2cee14fc4a99f367a25e232398f63d057b66
frankyoh <frankyoh@tmon.co.kr>
2020-05-21 22:42:34 +0900

    [QUOTE-5][FE] 명언 Carousel 컨포넌트 추가

---
+++ app/src/components/main/list/QuoteCarousel/QuoteCarousel.js
@@ -0,0 +1,58 @@
+import React, {useRef} from 'react';
+import styles from './QuoteCarousel.scss';
+import classNames from 'classnames/bind';
+import {Button, Carousel} from "antd";
+import {LeftCircleOutlined, RightCircleOutlined} from '@ant-design/icons';
+import _ from "lodash";
+
+const cx = classNames.bind(styles);
+
+const QuoteCarousel = ({quotes}) => {
+    const quoteView = [];
+
+    _(quotes).forEach(quoteInfo => {
+        quoteView.push(
+            <div key={quoteInfo.quoteId} className={cx('quote-item')}>
+                <blockquote>
+                    <p>{quoteInfo.quoteText}</p>
+                    <small>{quoteInfo.authorName}</small>
+                </blockquote>
+            </div>
+        )
+    });
+
+    const carouselRef = React.createRef();
+    const settings = {
+        dots: true,
+        infinite: true,
+        autoplaySpeed: 10000,
+        autoplay: true,
+        speed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
+    return (
+        <div>
+            <Carousel ref={carouselRef} {...settings}>
+                {quoteView}
+            </Carousel>
+            <Button type="link"
+                    className={cx('custom-slick-arrow')}
+                    style={{left: '10px', zIndex: '1'}}
+                    icon={<LeftCircleOutlined/>}
+                    onClick={() => {
+                        carouselRef.current.prev()
+                    }}/>
+            <Button type="link"
+                    className={cx('custom-slick-arrow')}
+                    style={{right: '10px'}}
+                    icon={<RightCircleOutlined/>}
+                    onClick={() => {
+                        carouselRef.current.next()
+                    }}/>
+        </div>
+
+    );
+};
+export default QuoteCarousel;
