642a969da19952b63b60685d4bcc1ddcd9437a21
frankyoh <frankyoh@tmon.co.kr>
2020-05-21 20:23:03 +0900

    [QUOTE-4][FE] font-awesome css import 시킴

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import Root from "./Root";
 import 'styles/base.scss';
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<Root/>, document.getElementById('root'));
 
