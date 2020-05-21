3da0c47bb07dff48404f70caa35af40039f5559a
frankyoh <frankyoh@tmon.co.kr>
2020-05-21 20:45:01 +0900

    [QUOTE-4][FE] 통계 Item 컨포넌트 추가

---
+++ app/src/components/user/list/StatisticItem/StatisticItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './StatisticItem.scss';
+import classNames from 'classnames/bind';
+import {Col, Divider} from 'antd';
+
+const cx = classNames.bind(styles);
+
+const StatisticItem = ({title, data}) => {
+    return (
+        <>
+            <Divider type='vertical' style={{marginLeft: '30px', color: '#b6b7ab', width: '2px', height: '70px', }}>
+            </Divider>
+            <Col span={6}>
+                <div className={cx('stat-title')}>{title}</div>
+                <div className={cx('stat-data')}>{data}</div>
+            </Col>
+        </>
+    );
+};
+export default StatisticItem;
