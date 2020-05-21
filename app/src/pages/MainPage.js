99e7e1bb837406ea3b4f31a2bc2d411e4f5f2320
frankyoh <frankyoh@tmon.co.kr>
2020-05-21 22:44:05 +0900

    [QUOTE-5][FE] 메인 첫 페이지 추가

--- app/src/pages/MainPage.js
+++ app/src/pages/MainPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PageTemplate from "../components/common/PageTemplate";
+import RandomListContainer from "../containers/main/RandomListContainer";
+import SearchContainer from "../containers/main/SearchContainer";
 
 const MainPage = () => {
     return (
         <PageTemplate>
-            <div>검색 창</div>
-            <div>오늘 명언 목록</div>
+            {/*<SearchContainer/>*/}
+            <RandomListContainer/>
         </PageTemplate>
     );
 };
