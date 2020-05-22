2c15fec61e362bbeadc5988c9373edbad2b733fc
frankyoh <frankyoh@tmon.co.kr>
2020-05-22 20:41:19 +0900

    [QUOTE-5][FE] 명언 Timeline 목록 컨포넌트 CSS 적용

--- app/src/components/main/list/QuoteTimelineList/QuoteTimelineList.js
+++ app/src/components/main/list/QuoteTimelineList/QuoteTimelineList.js
@@ -10,16 +10,16 @@ const cx = classNames.bind(styles);
 const QuoteTimelineList = ({quotes, loadMoreButton}) => {
     const quoteView = [];
 
-    // _(quotes).forEach(quoteInfo => {
-    //     quoteView.push(
-    //         <div key={quoteInfo.quoteId} className={cx('quote-item')}>
-    //             <blockquote>
-    //                 <p>{quoteInfo.quoteText}</p>
-    //                 <small>{quoteInfo.authorName}</small>
-    //             </blockquote>
-    //         </div>
-    //     )
-    // });
+    _(quotes).forEach(quoteInfo => {
+        quoteView.push(
+            <div key={quoteInfo.quoteId}>
+                <blockquote className={cx('otro-blockquote')}>
+                    {quoteInfo.quoteText}
+                    <small>{quoteInfo.authorName}</small>
+                </blockquote>
+            </div>
+        )
+    });
 
     return (
         <div className={cx('timeline-container')}>
