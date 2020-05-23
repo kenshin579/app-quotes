31bc2745f1859ab95b15cd8597369f08d3cada6e
frankyoh <frankyoh@tmon.co.kr>
2020-05-23 23:12:50 +0900

    [QUOTE-4][FE] folder action 수정 - folder 생성 추가

--- app/src/store/modules/folder.js
+++ app/src/store/modules/folder.js
@@ -5,9 +5,11 @@ import {pender} from "redux-pender";
 
 // action types
 const GET_FOLDER_LIST = 'folder/GET_FOLDER_LIST';
+const CREATE_FOLDER = 'quote/CREATE_FOLDER';
 
 // action creators
 export const getFolderList = createAction(GET_FOLDER_LIST, api.getFolderList);
+export const createFolder = createAction(CREATE_FOLDER, api.createFolder);
 
 // initial state
 const initialState = Map({
@@ -26,6 +28,12 @@ export default handleActions({
             const {folderList, folderStatInfo} = action.payload;
             return state.set('folders', fromJS(folderList))
                 .set('folderStatInfo', fromJS(folderStatInfo));
+        }
+    }),
+    ...pender({
+        type: CREATE_FOLDER,
+        onSuccess: (state, action) => {
+            console.log('successfully created a folder :: action.payload', action.payload);
         }
     })
 }, initialState)
