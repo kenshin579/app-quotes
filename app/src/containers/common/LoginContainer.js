b994581386d6f34ba01a2e577d4ead28c8bceeb8
frankyoh <frankyoh@tmon.co.kr>
2020-05-24 13:54:19 +0900

    [QUOTE-2][FE] notification msg bottomRight로 띄도록 옵션 수정

--- app/src/containers/common/LoginContainer.js
+++ app/src/containers/common/LoginContainer.js
@@ -20,15 +20,18 @@ class LoginContainer extends Component {
 
         } catch (err) {
             console.error('err', err);
+            //todo: notification 별도의 wrapper 컨포넌트 만들기
             if (err.status === 401) {
                 notification.error({
                     message: 'Quote App',
                     description: '아이디나 암호가 잘못 입력되었습니다. 다시 시도해주세요!',
+                    placement: 'bottomRight'
                 });
             } else {
                 notification.error({
                     message: 'Quote App',
                     description: '서버 오류가 발생했습니다. 다시 시도해주세요',
+                    placement: 'bottomRight'
                 });
             }
         }
