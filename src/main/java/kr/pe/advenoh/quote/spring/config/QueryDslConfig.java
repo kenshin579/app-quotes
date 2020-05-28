6e584d18ea056870ade0f595c4faff565b0922fa
frankyoh <frankyoh@tmon.co.kr>
2020-05-28 23:18:56 +0900

    [QUOTE-4][BE] querydsl sql 설정 추가

--- src/main/java/kr/pe/advenoh/quote/spring/config/QueryDslConfig.java
+++ src/main/java/kr/pe/advenoh/quote/spring/config/QueryDslConfig.java
@@ -1,19 +1,47 @@
 package kr.pe.advenoh.quote.spring.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.sql.MySQLTemplates;
+import com.querydsl.sql.SQLQueryFactory;
+import com.querydsl.sql.SQLTemplates;
+import com.querydsl.sql.spring.SpringConnectionProvider;
+import com.querydsl.sql.spring.SpringExceptionTranslator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.inject.Provider;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @Configuration
 public class QueryDslConfig {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(entityManager);
+    }
+
+    @Bean
+    public com.querydsl.sql.Configuration querydslConfiguration() {
+        SQLTemplates templates = MySQLTemplates.builder().build(); //change to your Templates
+        com.querydsl.sql.Configuration configuration = new com.querydsl.sql.Configuration(templates);
+        SpringExceptionTranslator springExceptionTranslator = new SpringExceptionTranslator();
+        configuration.setExceptionTranslator(springExceptionTranslator);
+        return configuration;
+    }
+
+    @Bean
+    public SQLQueryFactory queryFactory() {
+        Provider<Connection> provider = new SpringConnectionProvider(dataSource);
+        SQLQueryFactory sqlQueryFactory = new SQLQueryFactory(querydslConfiguration(), provider);
+        return sqlQueryFactory;
     }
 }
