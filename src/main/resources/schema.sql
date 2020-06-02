c1f1465ee859c99c7ea2ad37a146197002749c44
frankyoh <frankyoh@tmon.co.kr>
2020-06-02 21:32:32 +0900

    [QUOTE-6][BE] schema에 기본 데이터 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -162,3 +162,17 @@ create table users_roles
     constraint FKj6m8fwv7oqv74fcehir1a9ffy
         foreign key (role_id) references roles (role_id)
 ) ENGINE = InnoDB;
+
+
+INSERT INTO app_quotes.roles (role_id, role_type) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO app_quotes.roles (role_id, role_type) VALUES (2, 'ROLE_USER');
+
+INSERT INTO app_quotes.privileges (privilege_id, privilege_type) VALUES (1, 'READ_PRIVILEGE');
+INSERT INTO app_quotes.privileges (privilege_id, privilege_type) VALUES (2, 'WRITE_PRIVILEGE');
+INSERT INTO app_quotes.privileges (privilege_id, privilege_type) VALUES (3, 'CHANGE_PASSWORD_PRIVILEGE');
+
+INSERT INTO app_quotes.roles_privileges (role_id, privilege_id) VALUES (1, 1);
+INSERT INTO app_quotes.roles_privileges (role_id, privilege_id) VALUES (1, 2);
+INSERT INTO app_quotes.roles_privileges (role_id, privilege_id) VALUES (1, 3);
+INSERT INTO app_quotes.roles_privileges (role_id, privilege_id) VALUES (2, 1);
+INSERT INTO app_quotes.roles_privileges (role_id, privilege_id) VALUES (2, 3);
