90792f3e3907850e39ce28239a721dfd7ae2d933
frankyoh <frankyoh@tmon.co.kr>
2020-06-05 22:34:20 +0900

    [QUOTE-4][BE] 명언 API 목록 조회 querydsl sql 사용하지 않도록 다시 수정함

--- src/main/java/kr/pe/advenoh/quote/repository/quote/QuoteRepositoryCustomImpl.java
+++ src/main/java/kr/pe/advenoh/quote/repository/quote/QuoteRepositoryCustomImpl.java
@@ -1,22 +1,25 @@
 package kr.pe.advenoh.quote.repository.quote;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.querydsl.sql.SQLExpressions;
-import com.querydsl.sql.SQLQuery;
-import com.querydsl.sql.SQLQueryFactory;
 import kr.pe.advenoh.quote.model.dto.QuoteResponseDto;
-import kr.pe.advenoh.quote.model.entity.*;
-import kr.pe.advenoh.quote.model.enums.YN;
+import kr.pe.advenoh.quote.model.entity.QAuthor;
+import kr.pe.advenoh.quote.model.entity.QFolder;
+import kr.pe.advenoh.quote.model.entity.QFolderQuoteMapping;
+import kr.pe.advenoh.quote.model.entity.QQuote;
+import kr.pe.advenoh.quote.model.entity.QQuoteTagMapping;
+import kr.pe.advenoh.quote.model.entity.QTag;
+import kr.pe.advenoh.quote.model.entity.Quote;
+import kr.pe.advenoh.quote.model.entity.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -24,12 +27,10 @@ import java.util.stream.Collectors;
 @Slf4j
 public class QuoteRepositoryCustomImpl extends QuerydslRepositorySupport implements QuoteRepositoryCustom {
     private final JPAQueryFactory queryFactory;
-    private final SQLQueryFactory sqlQueryFactory;
 
-    public QuoteRepositoryCustomImpl(JPAQueryFactory queryFactory, SQLQueryFactory sqlQueryFactory) {
+    public QuoteRepositoryCustomImpl(JPAQueryFactory queryFactory) {
         super(Quote.class);
         this.queryFactory = queryFactory;
-        this.sqlQueryFactory = sqlQueryFactory;
     }
 
     /**
@@ -43,50 +44,36 @@ public class QuoteRepositoryCustomImpl extends QuerydslRepositorySupport impleme
      */
     @Override
     public Page<QuoteResponseDto> findAllByFolderId(Long folderId, Pageable pageable) {
-        SQuotes sQuotes = SQuotes.quotes;
-        SAuthors sAuthors = SAuthors.authors;
-        STags sTags = STags.tags;
-        SQuotesTags sQuotesTags = SQuotesTags.quotesTags;
-        SFolders sFolders = SFolders.folders;
-        SFoldersQuotes sFoldersQuotes = SFoldersQuotes.foldersQuotes;
+        QQuote qQuote = QQuote.quote;
+        QTag qTag = QTag.tag;
+        QAuthor qAuthor = QAuthor.author;
+        QFolder qFolder = QFolder.folder;
+        QFolderQuoteMapping qFolderQuoteMapping = QFolderQuoteMapping.folderQuoteMapping;
+        QQuoteTagMapping qQuoteTagMapping = QQuoteTagMapping.quoteTagMapping;
 
-        SQLQuery<String> subQuery = sqlQueryFactory.selectDistinct(SQLExpressions.groupConcat(sTags.tagName))
-                .from(sTags)
-                .innerJoin(sQuotesTags).on(sTags.tagId.eq(sQuotesTags.tagId))
-                .where(sQuotes.quoteId.eq(sQuotesTags.quoteId));
-
-        long totalElements = sqlQueryFactory.query()
-                .from(sQuotes)
-                .fetchCount();
-
-        //todo : orderby pageable 인자로 넘겨준 값으로 정렬하도록 수정 필요함
-
-        List<QuoteResponseDto> quoteResponseDtos = sqlQueryFactory.select(sQuotes.quoteId, sQuotes.quoteText, sQuotes.useYn, sAuthors.name, subQuery.as("tagList"))
-                .from(sQuotes)
-                .innerJoin(sFolders)
-                .innerJoin(sFoldersQuotes).on(sFoldersQuotes.folderId.eq(sFolders.folderId))
-//                .leftJoin(sQuotesTags).on(sQuotes.quoteId.eq(sQuotesTags.quoteId))
-                .innerJoin(sAuthors).on(sQuotes.authorId.eq(sAuthors.authorId))
-                .where(sFolders.folderId.eq(folderId).and(sFoldersQuotes.quoteId.eq(sQuotes.quoteId)))
+        QueryResults<QuoteResponseDto> queryResults = queryFactory.select(Projections.fields(QuoteResponseDto.class,
+                qQuote.id.as("quoteId"), qQuote.quoteText, qQuote.author.name.as("authorName"), qQuote.useYn))
+                .from(qQuote)
+                .innerJoin(qFolder).on(qFolder.id.eq(folderId))
+                .innerJoin(qFolderQuoteMapping).on(qFolderQuoteMapping.quote.id.eq(qQuote.id)
+                        .and(qFolder.id.eq(qFolderQuoteMapping.folder.id)))
+                .innerJoin(qAuthor).on(qAuthor.id.eq(qQuote.author.id))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
-                .orderBy(new OrderSpecifier(Order.DESC, sQuotes.createDt))
-                .fetch().stream()
-                .map(x -> {
-                    QuoteResponseDto quoteResponseDto = QuoteResponseDto.builder()
-                            .quoteId(x.get(sQuotes.quoteId))
-                            .quoteText(x.get(sQuotes.quoteText))
-                            .useYn(YN.valueOf(x.get(sQuotes.useYn)))
-                            .build();
-                    Optional.ofNullable(x.get(sAuthors.name)).ifPresent(quoteResponseDto::setAuthorName);
-                    Optional.ofNullable(x.get(x.size() - 1, String.class)).ifPresent(y -> quoteResponseDto.setTags(Arrays.asList(y.split(","))));
-                    return quoteResponseDto;
-                })
-                .collect(Collectors.toList());
+                .orderBy(new OrderSpecifier(Order.DESC, qQuote.createDt))
+                .fetchResults();
 
-        log.info("quoteResponseDtos : {}", quoteResponseDtos);
-
-        return new PageImpl<>(quoteResponseDtos, pageable, totalElements);
+        List<QuoteResponseDto> quoteResponseDtos = queryResults.getResults().stream()
+                .map(it -> {
+                    List<Tag> tags = queryFactory.select(Projections.constructor(Tag.class, qTag.tagName))
+                            .from(qTag)
+                            .innerJoin(qQuoteTagMapping).on(qTag.id.eq(qQuoteTagMapping.tag.id))
+                            .where(qQuoteTagMapping.quote.id.eq(it.getQuoteId()))
+                            .fetch();
+                    it.setTags(tags.stream().map(Tag::getTagName).collect(Collectors.toList()));
+                    return it;
+                }).collect(Collectors.toList());
+        return new PageImpl<>(quoteResponseDtos, pageable, queryResults.getTotal());
     }
 
     /**
