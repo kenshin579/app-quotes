3b9f40048513c629b7a00aa466b5d25e8c17c0d4
frankyoh <frankyoh@tmon.co.kr>
2020-06-12 07:59:03 +0900

    [QUOTE-6][BE] profile API 추가

--- src/main/java/kr/pe/advenoh/quote/spring/config/SecurityConfig.java
+++ src/main/java/kr/pe/advenoh/quote/spring/config/SecurityConfig.java
@@ -73,7 +73,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.jpg",
                         "/**/*.html",
                         "/**/*.css",
-                        "/**/*.js")
+                        "/**/*.js",
+                        "/profile")
                 .permitAll()
                 .antMatchers("/api/auth/**", "/actuator/**")
                 .permitAll()
