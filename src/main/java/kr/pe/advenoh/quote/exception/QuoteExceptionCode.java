622d0a85246cc1748735f6fbdbcc02282ad6ce6b
frankyoh <frankyoh@tmon.co.kr>
2020-08-01 12:56:46 +0900

    [QUOTE-34][BE] 예외 메시지 영어 -> 한글로 변경함

--- src/main/java/kr/pe/advenoh/quote/exception/QuoteExceptionCode.java
+++ src/main/java/kr/pe/advenoh/quote/exception/QuoteExceptionCode.java
@@ -12,10 +12,10 @@ public enum QuoteExceptionCode implements ExceptionCode {
     system  : 50000
     */
 
-    ACCOUNT_USER_NOT_FOUND("10000", "User not found with username or email : %s"),
-    ACCOUNT_USERNAME_IS_ALREADY_EXIST("10010", "Username is already exist!"),
-    ACCOUNT_EMAIL_IS_ALREADY_EXIST("10020", "Email is already exist!"),
-    ACCOUNT_USER_REGISTERED_SUCCESS("10030", "User registered successfully"),
+    ACCOUNT_USER_NOT_FOUND("10000", "사용자 정보를 찾을 수 없습니다"),
+    ACCOUNT_USERNAME_IS_ALREADY_EXIST("10010", "Username이 이미 존재합니다"),
+    ACCOUNT_EMAIL_IS_ALREADY_EXIST("10020", "이메일 주소가 이미 존재합니다"),
+    ACCOUNT_USER_REGISTERED_SUCCESS("10030", "사용자 등록이 완료되었습니ㄷ"),
     ACCOUNT_ROLE_NOT_FOUND("10040", "Role(%s) 정보를 찾을 수가 없습니다"),
     ACCOUNT_FAILED_LOGIN("10050", "id/password를 확인해주세요"),
 
