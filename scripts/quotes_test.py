7f515961ca47449674053af262a40739624379b1
kenshin579 <kenshin579@hotmail.com>
2020-11-14 06:37:40 +0900

    [QUOTE-70] munganbot에 맞는 postprocess 작성 완료

--- scripts/quotes_test.py
+++ scripts/quotes_test.py
@@ -4,8 +4,13 @@ import quotes
 
 
 class QuotesTest(TestCase):
-    def test_postprocess(self):
-        print('test')
-        result = quotes.postprocess('test')
-        print('result', result)
-        # self.fail()
+    def test_postprocess_munganbot(self):
+        result = quotes.postprocess('봇의특성상트윗이중복될수있습니다')
+        self.assertEqual(result, '')
+
+    def test_postprocess_munganbot2(self):
+        result = quotes.postprocess('* text.')
+        self.assertEqual(result, 'text')
+
+        result = quotes.postprocess('* 충고는 눈과 같아 조용히 내리면 내릴수록 마음에 오래 남고 가슴에 더욱 깊이 새겨진다.')
+        self.assertEqual(result, '충고는 눈과 같아 조용히 내리면 내릴수록 마음에 오래 남고 가슴에 더욱 깊이 새겨진다')
