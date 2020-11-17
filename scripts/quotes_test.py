07e4bf1f86c8c727d5fe0b60b6bd127958bfe9a9
kenshin579 <kenshin579@hotmail.com>
2020-11-17 19:46:39 +0900

    [QUOTE-70] parse_quote 관련 unit test 추가

--- scripts/quotes_test.py
+++ scripts/quotes_test.py
@@ -23,6 +23,10 @@ class QuotesTest(TestCase):
         result = quotes.postprocess('text.   ')
         self.assertEqual('text', result)
 
+    def test_postprocess_munganbot5(self):
+        result = quotes.postprocess('text    ')
+        self.assertEqual('text', result)
+
     def test_parse_quote(self):
         result = quotes.parse_quote('사랑은 있거나, 없다.\n(둘 중 하나다.) 가벼운 사랑은 아예 사랑이 아니다 \n\n\n -토니 모리슨\n')
         self.assertEqual('사랑은 있거나, 없다.\n(둘 중 하나다.) 가벼운 사랑은 아예 사랑이 아니다 \n\n\n ', result[0])
