4030ca1f6c13d3c1f65560d5df33dd4eba4e6e1f
kenshin579 <kenshin579@hotmail.com>
2020-12-31 10:28:48 +0900

    [QUOTE-57] QuoteTag -> 다대다 연관관계 변경에 따른 폴더 삭제 로직 수정

--- src/test/java/kr/pe/advenoh/admin/folder/service/FolderServiceMockTest.java
+++ src/test/java/kr/pe/advenoh/admin/folder/service/FolderServiceMockTest.java
@@ -5,7 +5,7 @@ import kr.pe.advenoh.admin.folder.domain.FolderRepository;
 import kr.pe.advenoh.admin.folder.domain.FolderUserMappingRepository;
 import kr.pe.advenoh.admin.quote.domain.QuoteHistoryRepository;
 import kr.pe.advenoh.admin.quote.domain.QuoteRepository;
-import kr.pe.advenoh.admin.quote.domain.QuoteTagMappingRepository;
+import kr.pe.advenoh.admin.quote.service.QuoteService;
 import kr.pe.advenoh.util.MockitoTestSupport;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -28,32 +28,25 @@ class FolderServiceMockTest extends MockitoTestSupport {
     private FolderQuoteMappingRepository folderQuoteMappingRepository;
 
     @Mock
-    private QuoteTagMappingRepository quoteTagMappingRepository;
-
-    @Mock
-    private QuoteHistoryRepository quoteHistoryRepository;
-
-    @Mock
-    private QuoteRepository quoteRepository;
-
-    @Mock
     private FolderUserMappingRepository folderUserMappingRepository;
 
     @Mock
     private FolderRepository folderRepository;
 
+    @Mock
+    private QuoteService quoteService;
+
     @Test
     void deleteFolders_folder안에_명언이_없는_경우() {
         //given
         List<Long> folderIds = Arrays.asList(1L);
-        //		List<Long> quoteIds = Arrays.asList(10L, 20L);
         when(folderQuoteMappingRepository.getAllQuoteIdsByFolderIds(folderIds)).thenReturn(Collections.EMPTY_LIST);
 
         //when
         folderService.deleteFolders(folderIds);
 
         //then
-        verify(quoteRepository, never()).deleteAllByQuoteIds(anyList());
+        verify(quoteService, never()).deleteQuotes(anyList());
         verify(folderQuoteMappingRepository).deleteAllByFolderIds(folderIds);
         verify(folderUserMappingRepository).deleteAllByFolderIds(folderIds);
         verify(folderRepository).deleteAllByFolderIds(folderIds);
@@ -72,11 +65,7 @@ class FolderServiceMockTest extends MockitoTestSupport {
 
         //then
         verify(folderQuoteMappingRepository).deleteAllByFolderIds(folderIds);
-
-        verify(quoteTagMappingRepository).deleteAllByQuoteIds(quoteIds);
-        verify(quoteHistoryRepository).deleteAllByQuoteIds(quoteIds);
-        verify(quoteRepository).deleteAllByQuoteIds(quoteIds);
-
+        verify(quoteService).deleteQuotes(quoteIds);
         verify(folderUserMappingRepository).deleteAllByFolderIds(folderIds);
         verify(folderRepository).deleteAllByFolderIds(folderIds);
         verify(folderQuoteMappingRepository).getAllQuoteIdsByFolderIds(anyList());
