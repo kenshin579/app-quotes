17e3eb3c00fa2460748883203178b0119967f47f
kenshin579 <kenshin579@hotmail.com>
2021-03-22 23:08:27 +0900

    [QUOTE-170] app 구동시 환경변수 출력하는 eventlistener 추가

---
+++ src/main/java/kr/pe/advenoh/spring/listener/AppContextEventListener.java
@@ -0,0 +1,49 @@
+package kr.pe.advenoh.spring.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Slf4j
+@Component
+public class AppContextEventListener {
+
+    @EventListener
+    public void handleContextRefreshed(ContextRefreshedEvent event) {
+        printActiveProperties((ConfigurableEnvironment) event.getApplicationContext().getEnvironment());
+    }
+
+    private void printActiveProperties(ConfigurableEnvironment env) {
+
+        log.info("************************* ACTIVE APP PROPERTIES ******************************");
+
+        List<MapPropertySource> propertySources = new ArrayList<>();
+
+        env.getPropertySources().forEach(it -> {
+            if (it instanceof MapPropertySource && it.getName().contains("applicationConfig")) {
+                propertySources.add((MapPropertySource) it);
+            }
+        });
+
+        propertySources.stream()
+                .map(propertySource -> propertySource.getSource().keySet())
+                .flatMap(Collection::stream)
+                .distinct()
+                .sorted()
+                .forEach(key -> {
+                    try {
+                        log.info(key + "=" + env.getProperty(key));
+                    } catch (Exception e) {
+                        log.warn("{} -> {}", key, e.getMessage());
+                    }
+                });
+        log.info("******************************************************************************");
+    }
+}
