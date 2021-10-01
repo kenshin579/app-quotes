32ad3d739bb0462c36217cdcf64c17892590dfc2
kenshin579 <kenshin579@hotmail.com>
2021-10-01 23:18:26 +0900

    [QUOTE-173] 루트 기본 설정

---
+++ my-app/src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                BlogApp
+            </div>
+        );
+    }
+}
+
+export default App;
