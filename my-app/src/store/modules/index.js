53cccc9b92a66a2bf6702421556fc6606a2bcee6
kenshin579 <kenshin579@hotmail.com>
2021-10-02 11:06:42 +0900

    [QUOTE-173] myapp 삭제

--- my-app/src/store/modules/index.js
+++
@@ -1,5 +0,0 @@
-export {default as base} from './base';
-export {default as quote} from './quote';
-export {default as folder} from './folder';
-export {default as main} from './main';
-export {penderReducer as pender} from 'redux-pender';
--- my-app/src/utils/numberformat.js
+++
@@ -1,8 +0,0 @@
-
-const objectToQueryString = (obj) => {
-    return Object.keys(obj).map(key => key + '=' + obj[key]).join('&');
-};
-
-export const convertNumberWithComma = (value) => {
-    return value.toLocaleString(navigator.language, { minimumFractionDigits: 0 });
-};
